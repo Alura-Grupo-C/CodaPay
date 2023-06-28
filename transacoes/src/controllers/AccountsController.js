93a8c4b934c84f200fc52c4bf49e9b3a632333ac
pdragoni <n134441@gruposantander.com>
2023-06-28 17:50:41 -0300

    [feat] transacoes: implementação, autenticação e autorização

--- transacoes/src/controllers/AccountsController.js
+++ transacoes/src/controllers/AccountsController.js
@@ -1,6 +1,6 @@
 import Account from '../models/Account.js';
 import criptografia from '../../middlewares/bcryptMiddleware.js';
-
+import tokenHandler from '../../middlewares/jwtMiddleware.js';
 
 class AccountController {
   static findAccounts = (_req, res) => {
@@ -38,6 +38,20 @@ class AccountController {
       }
       return res.status(201).set('Location', `/admin/accounts/${account.id}`).json(newAccount);
     });
+  };
+
+  static loginAccount = async (req, res) => {
+    try {
+      const { email, senha } = req.body;
+      const senhaProvaReal = await criptografia.protegeSenha(senha);
+      const account = await Account.findOne({ email, senhaProvaReal }).exec();
+      const serverResponse = tokenHandler.tokenCreator(account.id);
+      req.headers.authorization = serverResponse;
+      return res.status(204).json();
+    } catch (err) {
+      console.log(`catch error:, ${err}`);
+    }
+    return res.status(400).json({ message: 'Bad Request. User not found' });
   };
 
   static updateAccount = (req, res) => {
