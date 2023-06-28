f935b4ee66fb05661237fb3873a336924aacaceb
pdragoni <n134441@gruposantander.com>
2023-06-28 17:23:27 -0300

    [feat] clientes: implementação de login, autenticação e autorização

--- clientes/src/controllers/AccountsController.js
+++ clientes/src/controllers/AccountsController.js
@@ -1,5 +1,6 @@
 import Account from '../models/Account.js';
 import criptografia from '../../middlewares/bcryptMiddleware.js';
+import tokenHandler from '../../middlewares/jwtMiddleware.js';
 
 class AccountController {
   static findAccounts = (_req, res) => {
@@ -39,6 +40,20 @@ class AccountController {
     });
   };
 
+  static loginAccount = async (req, res) => {
+    try {
+      const { email, senha } = req.body;
+      const senhaProvaReal = await criptografia.protegeSenha(senha);
+      const account = await Account.findOne({ email, senhaProvaReal }).exec();
+      const serverResponse = tokenHandler.tokenCreator(account.id);
+      req.headers.authorization = serverResponse;
+      return res.status(204).json();
+    } catch (err) {
+      console.log(`catch error:, ${err}`);
+    }
+    return res.status(400).json({ message: 'Bad Request. User not found' });
+  };
+
   static updateAccount = (req, res) => {
     const { id } = req.params;
 
@@ -46,7 +61,7 @@ class AccountController {
       if (err) {
         return res.status(500).send({ message: err.message });
       }
-      return res.status(204).set('Location', `/admin/accounts/${account.id}`).send();
+      return res.status(204).set('Location', `/ admin / accounts / ${account.id}`).send();
     });
   };
 
