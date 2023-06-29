11c5e4e44cf8adbf92291fa961481feab0eb0f41
elizarraga <eduardo.alejandro.lizarraga@gmail.com>
2023-06-28 22:57:31 -0300

    Endpoint(s) para dados pessoais de um cliente

--- clientes/src/controllers/ClientesController.js
+++ clientes/src/controllers/ClientesController.js
@@ -14,6 +14,31 @@ class ClienteController{
             return res.status(201).set('Location', `/admin/accounts/${cliente.id}`).json(newCliente);
           });
     }
+
+    static findClientes = async (req, res) => {
+        try{
+          const pesquisa = await Cliente.find()
+          res.status(200).json({Clientes: pesquisa});
+        }catch(erro){
+          res.status(500).json({message: erro});
+        }
+    }
+
+    static findClienteById = async (req, res) => {
+      const {id} = req.params
+      try{
+          const cliente = await Cliente.findById(id)
+          if(cliente){
+              const dadosPessoais = {dadosPessoais: cliente.dadosPessoais}
+              const endereco = {endereco: cliente.endereco}
+              res.status(200).json({Cliente: [dadosPessoais, endereco]})
+          }else{
+              res.status(404).send({message: "Cliente não encontrada"})
+          }
+      }catch(erro){
+          res.status(500).send({message: erro.message});
+      }        
+    }
 }
 
 export default ClienteController;
--- clientes/src/routes/clientesRoutes.js
+++ clientes/src/routes/clientesRoutes.js
@@ -4,7 +4,9 @@ import ClienteController from '../controllers/ClientesController.js'
 const router = express.Router();
 
 router
-    .post('/api/admin/cliente', ClienteController.createCliente)
+    .get('/api/admin/clientes', ClienteController.findClientes)
+    .get('/api/admin/clientes/:id', ClienteController.findClienteById)
+    .post('/api/admin/clientes', ClienteController.createCliente)
 
 
 export default router;
