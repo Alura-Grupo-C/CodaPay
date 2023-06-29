e0bef5c77857e51f04ba39b38f4ef909252175d4
Barbara Luz <barbarartluz@gmail.com>
2023-06-29 15:45:43 -0300

    Revert "Create transaction"

--- anti-fraude/src/routes/AnaliseAntiFraudeRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import AnaliseAntiFraudeController from '../controllers/AnaliseAntiFraudeController.js';
-
-const router = express.Router();
-
-router
-  .post('/api/admin/antifraude', AnaliseAntiFraudeController.criarAnalise);
-
-export default router;
